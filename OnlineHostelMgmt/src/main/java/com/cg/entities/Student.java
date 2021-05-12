41920e0662ea6d213f9841e6b07fee81a0921361
supriyo15998 <supriyo15998@gmail.com>
2021-05-12 19:41:04 +0530

    student entity - supriyo

--- OnlineHostelMgmt/src/main/java/com/cg/entities/Student.java
+++ OnlineHostelMgmt/src/main/java/com/cg/entities/Student.java
@@ -1,5 +1,24 @@
 package com.cg.entities;
 
-public class Student {
+import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "students")
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	private String email;
+	private String gender;
+	private LocalDate dob;
+	private String mobile;
+	private String address;
+	private Room room;
+	private String guardianName;
 }
