42901a3eb33bb08dd2a289eba5678dfa1f980bf2
Dell Pc <Dell Pc@Dell>
2021-05-12 20:56:23 +0530

    Hostel class - Sameema

---
+++ OnlineHostelMgmt/src/main/java/com/cg/entities/Hostel.java
@@ -0,0 +1,122 @@
+package com.cg.entities;
+
+import java.util.Set;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "hostel")
+public class Hostel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "seq")
+	@SequenceGenerator(name = "seq", sequenceName = "hostel_seq", allocationSize = 1)
+	private Long id;
+	
+	private String name;
+	private String contact;
+	private String type;
+	private String address;
+	private Double fee;
+	
+	private Warden warden;
+	
+	@OneToMany(mappedBy = "")
+	@JsonIgnore
+	private Set<Room> rooms;
+	
+	
+	//Getters and Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Double getFee() {
+		return fee;
+	}
+
+	public void setFee(Double fee) {
+		this.fee = fee;
+	}
+
+	public Warden getWarden() {
+		return warden;
+	}
+
+	public void setWarden(Warden warden) {
+		this.warden = warden;
+	}
+
+	public Set<Room> getRooms() {
+		return rooms;
+	}
+
+	public void setRooms(Set<Room> rooms) {
+		this.rooms = rooms;
+	}
+
+	
+	//Constructors
+	public Hostel() {
+		super();
+	
+	}
+
+	public Hostel(Long id, String name, String contact, String type, String address, Double fee, Warden warden, Set<Room> rooms) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.contact = contact;
+		this.type = type;
+		this.address = address;
+		this.fee = fee;
+		this.warden = warden;
+		this.rooms = rooms;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return id + " " + name + " " + contact + " " + type + " " + address + " " + fee;
+	}
+	
+}
