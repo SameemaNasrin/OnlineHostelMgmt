354b3a03bdcdede3683dfa6d911f04a50de3e8fb
supriyo15998 <supriyo15998@gmail.com>
2021-05-12 19:57:57 +0530

    changes

--- OnlineHostelMgmt/src/main/java/com/cg/entities/Student.java
+++ OnlineHostelMgmt/src/main/java/com/cg/entities/Student.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 @Entity
 @Table(name = "students")
 public class Student {
@@ -21,4 +22,102 @@ public class Student {
 	private String address;
 	private Room room;
 	private String guardianName;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int id, String name, String email, String gender, LocalDate dob, String mobile, String address,
+			Room room, String guardianName) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.gender = gender;
+		this.dob = dob;
+		this.mobile = mobile;
+		this.address = address;
+		this.room = room;
+		this.guardianName = guardianName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+
+	public String getGuardianName() {
+		return guardianName;
+	}
+
+	public void setGuardianName(String guardianName) {
+		this.guardianName = guardianName;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", email=" + email + ", gender=" + gender + ", dob=" + dob
+				+ ", mobile=" + mobile + ", address=" + address + ", guardianName=" + guardianName + "]";
+	}
+
 }
