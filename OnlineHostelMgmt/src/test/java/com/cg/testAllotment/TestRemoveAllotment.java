df012d8b2ef6991f28067fedea29b115691f6b28
user <user@192.168.0.7>
2021-05-22 17:07:55 +0530

    test-remove allotment is pending-Raza

--- OnlineHostelMgmt/src/test/java/com/cg/testAllotment/TestRemoveAllotment.java
+++ OnlineHostelMgmt/src/test/java/com/cg/testAllotment/TestRemoveAllotment.java
@@ -12,11 +12,14 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.cg.dao.IAllotmentDao;
+import com.cg.dao.IFeeStructureDao;
 import com.cg.dao.IHostelDao;
 import com.cg.dao.IRoomDao;
 import com.cg.dao.IStudentDao;
 import com.cg.entities.Allotment;
 import com.cg.entities.Room;
+import com.cg.exceptions.AllotmentNotFoundException;
+import com.cg.exceptions.RoomNotFoundException;
 import com.cg.services.AllotmentServiceImpl;
 import com.cg.services.IAllotmentService;
 
@@ -30,13 +33,11 @@ public class TestRemoveAllotment {
 	@Mock
 	private IRoomDao roomDao;
 	@Mock
-	private IHostelDao hostelDao;
-	@Mock
-	private IStudentDao studentDao;
+	private IFeeStructureDao feeDao;
 	
 	@BeforeEach
-	public void beforeEach() {
-		Room room1=new Room();
+	public void beforeEach()throws AllotmentNotFoundException,RoomNotFoundException {
+		/*Room room1=new Room();
 		room1.setRoomId(101);
 		room1.setMaximumSize(4);
 		Allotment allotment1=new Allotment();
@@ -44,8 +45,16 @@ public class TestRemoveAllotment {
 		allotment1.setRoom(room1);
 		List<Allotment> lstallotment=new ArrayList<>();
 		lstallotment.add(allotment1);
+		*/
 		
-
+		Optional<Allotment> allot1 = Optional.of(new Allotment());
+		when(allotmentDao.findById(1001)).thenReturn(allot1);
+		when(allotmentDao.findById(1003)).thenReturn(allot1);
+		Optional<Allotment> allot2 = Optional.empty();
+		when(allotmentDao.findById(1002)).thenReturn(allot2);
+		Room room=new Room();
+		room.setRoomId(100);
+		room.setMaximumSize(3);
 		
 		
 		
