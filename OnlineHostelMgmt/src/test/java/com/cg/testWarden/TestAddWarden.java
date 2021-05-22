a83128da1deb3adcaa1eeaaf609d2351a7496666
user <user@192.168.0.7>
2021-05-22 15:57:27 +0530

    add warden-shadman

--- OnlineHostelMgmt/src/test/java/com/cg/testWarden/TestAddWarden.java
+++ OnlineHostelMgmt/src/test/java/com/cg/testWarden/TestAddWarden.java
@@ -7,21 +7,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.cg.dao.IHostelDao;
 import com.cg.dao.IWardenDao;
+import com.cg.entities.Hostel;
+import com.cg.entities.Warden;
 import com.cg.services.IWardenService;
 import com.cg.services.WardenServiceImpl;
 
 @SpringBootTest
-public class TestAddWarden{
-	
+public class TestAddWarden {
+
 	@Mock
 	private IWardenDao wardenDao;
 	@Mock
 	private IHostelDao hostelDao;
 	@InjectMocks
-	private IWardenService service=new WardenServiceImpl();
+	private IWardenService service = new WardenServiceImpl();
+	
 	@BeforeEach
 	public void beforeEach() {
 		
+		Hostel hostel1=new Hostel();
+		hostel1.setId(5001);
+		Warden warden1=new Warden();
+		warden1.setId(100);
 		
+
 	}
 }
