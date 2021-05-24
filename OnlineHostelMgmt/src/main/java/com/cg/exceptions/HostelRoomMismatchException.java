749c62b620827e46a6c27162992adcfb216770c6
nasri <nasri@DESKTOP-9LI9SKN>
2021-05-24 06:50:42 +0530

    HostelRoomMismatchException added - Sameema

---
+++ OnlineHostelMgmt/src/main/java/com/cg/exceptions/HostelRoomMismatchException.java
@@ -0,0 +1,13 @@
+package com.cg.exceptions;
+
+public class HostelRoomMismatchException extends Exception {
+
+	public HostelRoomMismatchException() {
+		super();
+	}
+
+	public HostelRoomMismatchException(String message) {
+		super(message);
+	}
+
+}
